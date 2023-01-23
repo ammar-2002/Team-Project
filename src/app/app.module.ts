06cb0544620ff4a71f93e29cfef06363306cc75f
ammar-2002 <ammarmasselhy7@gmail.com>
2023-01-23 16:43:06 +0200

    Creating Componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
+import { HomeComponent } from './home/home.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    LoginSignupComponent,
+    HomeComponent,
+    CreatePostComponent,
+    NotificationsComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
